94e994ab7ae8b2805218b4c275269d1b7c831c1f
Ludmylla Arielly <ludmay4@gmail.com>
2020-12-04 22:20:24 -0300

    First commit

---
+++ src/main/java/com/ludmylla/personal/account/api/PersonalAccountApplication.java
@@ -0,0 +1,13 @@
+package com.ludmylla.personal.account.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ludmylla/personal/account/api/PersonalAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludmylla.personal.account.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
